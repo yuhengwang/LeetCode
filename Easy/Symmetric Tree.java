73199d0aef30d1e95326c9cc7e708c3cbf10b9a3
yuhengwang <yuhengw@andrew.cmu.edu>
2016-12-15 09:52:00 -0500

    Update Symmetric Tree.java

--- Easy/Symmetric Tree.java
+++ Easy/Symmetric Tree.java
@@ -1,3 +1,5 @@
+// recursion
+
 public class Solution {
     public boolean isSymmetric(TreeNode root) {
         
@@ -23,4 +25,69 @@ public class Solution {
         
         
     }
+}
+
+// using stack
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+      
+      // using stack
+      Stack<TreeNode> globalStack = new Stack <TreeNode>();
+      TreeNode left;
+      TreeNode right;
+      if(root == null) 
+        return true;
+      if(root.left != null)
+      {
+          
+        if(root.right == null )
+           return false;
+        else
+           globalStack.push(root.right);
+           globalStack.push(root.left);
+        
+    }else{
+        if(root.right != null )
+        return false;
+    }
+    
+    while(!globalStack.isEmpty()){
+        
+        if(globalStack.size()%2 != 0)
+           return false;
+        
+        left = globalStack.pop();
+        right = globalStack.pop();
+        
+        if(left.val!=right.val)
+          return false;
+        if(left.left != null){
+            if(right.right == null)
+               return false;
+            else
+               globalStack.push(left.left);
+               globalStack.push(right.right);
+        }else{
+            if(right.right != null)
+            return false;
+        }
+        
+        if(left.right != null){
+            if(right.left == null){
+                return false;
+            }
+            else
+              globalStack.push(left.right);
+              globalStack.push(right.left);
+        }else{
+            if(right.left != null)
+                return false;
+        }
+        
+        
+    }
+    
+    return true;
+    }  
+ 
 }
