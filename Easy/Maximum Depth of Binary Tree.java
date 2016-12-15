e18d3db6a9f6427d7e953a9cc8957bd9fb096a93
yuhengwang <yuhengw@andrew.cmu.edu>
2016-12-15 15:57:48 -0500

    Update Maximum Depth of Binary Tree.java

--- Easy/Maximum Depth of Binary Tree.java
+++ Easy/Maximum Depth of Binary Tree.java
@@ -10,3 +10,44 @@ public class Solution {
         return 1+ Math.max(maxDepth(root.left),maxDepth(root.right));
     }
 }
+
+
+//DFS
+public class Solution {
+    public int maxDepth(TreeNode root) {
+ 
+       //DFS
+       Stack <TreeNode> stack = new Stack <TreeNode> ();
+       Stack <Integer> value = new Stack <Integer> ();
+       
+       if( root == null) return 0;
+       
+       stack.push(root);
+       value.push(new Integer(1));
+       
+       int max = 0;
+       
+       while(!stack.isEmpty()){
+           TreeNode temp = stack.pop();
+           
+           int tempValue = value.pop();
+           max = Math.max(max,tempValue);
+           
+           if(temp.left != null){
+               
+               value.push(new Integer(tempValue+1));
+               stack.push(temp.left);
+               
+           }
+           
+           if(temp.right != null){
+               value.push(new Integer (tempValue+1));
+               stack.push(temp.right);
+           }
+           
+           
+       }
+       
+       return max;
+    }
+}
