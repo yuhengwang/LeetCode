5accb9cba4aa53c580eaa03b2e52adc0a5e5aa4d
yuhengwang <yuhengw@andrew.cmu.edu>
2016-12-16 14:37:25 -0500

    Update balanced binary Tree.java

--- Easy/balanced binary Tree.java
+++ Easy/balanced binary Tree.java
@@ -30,3 +30,47 @@ public class Solution {
         
     }
 }
+
+//another solution
+public class Solution {
+    
+     public int Depth(TreeNode root) {
+
+        if(root == null)
+          return 0;
+    
+        int left = Depth(root.left);
+    
+        if(left == -1)  return -1;
+        
+        int right = Depth(root.right);
+       
+        if(right == -1)  return -1;
+        
+        if(Math.abs(left-right)>1)
+         return -1;
+         
+        return Math.max(left,right)+1;
+        
+        
+      
+    }
+    
+    
+    public boolean isBalanced(TreeNode root) {
+        
+        if(root == null)
+          return true;
+       
+   
+       
+       
+       return Depth(root)!=-1;
+      
+       
+        
+        
+        
+        
+    }
+}
