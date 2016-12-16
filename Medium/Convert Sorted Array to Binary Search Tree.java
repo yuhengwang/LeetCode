9e7420673e2697e93ea5efd3b9117152dac049db
yuhengwang <yuhengw@andrew.cmu.edu>
2016-12-16 11:43:48 -0500

    Update Convert Sorted Array to Binary Search Tree.java

--- Medium/Convert Sorted Array to Binary Search Tree.java
+++ Medium/Convert Sorted Array to Binary Search Tree.java
@@ -25,3 +25,72 @@ public class Solution {
         
     }
 }
+
+
+// iterative
+public class Solution {
+    
+    public TreeNode sortedArrayToBST(int[] nums) {
+        
+        if (nums.length==0) return null;
+       
+        Stack <TreeNode> stack = new Stack <TreeNode> ();
+        Stack <Integer> leftIndex= new Stack <Integer> ();
+        Stack <Integer> rightIndex = new Stack <Integer> ();
+        
+        
+        TreeNode first = new TreeNode(0);
+        stack.push(first);
+        leftIndex.push(0);
+        rightIndex.push(nums.length-1);
+        
+        while(!stack.isEmpty()) {
+            
+            //low half of the array
+            TreeNode current = stack.pop();
+            int left = leftIndex.pop();
+            int right = rightIndex.pop();
+            
+            int index=  left + (right-left)/2;
+            
+            System.out.println(index);
+            
+            current.val = nums[index];
+            
+            if(index-1>=left){
+                
+                current.left = new TreeNode(0);
+                stack.push(current.left);
+                leftIndex.push(left);
+                rightIndex.push(index-1);
+                
+                
+            }
+            
+            
+            if(right>=index+1) {
+                
+                current.right = new TreeNode(0);
+                stack.push(current.right);
+                leftIndex.push(index+1);
+                rightIndex.push(right);
+                
+            }
+            
+            
+            
+            
+            
+            
+            
+            
+        }
+      
+      
+      return first;
+        
+        
+    }
+    
+  
+}
