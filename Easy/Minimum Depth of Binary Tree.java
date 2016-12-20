064aa8e2002b813d844bf6a47b524f8a7d38fea6
yuhengwang <yuhengw@andrew.cmu.edu>
2016-12-20 10:14:33 -0500

    Update Minimum Depth of Binary Tree.java

--- Easy/Minimum Depth of Binary Tree.java
+++ Easy/Minimum Depth of Binary Tree.java
@@ -1,4 +1,30 @@
 public class Solution {
+    public int helper(TreeNode root){
+        if(root == null) {
+            return Integer.MAX_VALUE;
+        }
+        
+        if(root.left == null && root.right == null) {
+            return 1;
+        }
+        
+       return Math.min(helper(root.left),helper(root.right))+1;
+        
+    }
+    
+    public int minDepth(TreeNode root) {
+        
+        //dfs
+        if(root == null) {
+            return 0;
+        }
+        
+        return helper(root);}
+}
+
+
+
+public class Solution {
     public int minDepth(TreeNode root) {
         
           Queue <TreeNode> queue = new LinkedList <TreeNode> ();
