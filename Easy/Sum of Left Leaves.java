64b2a9dd1c613d537250f8b4f5b0b8b5f60f64da
yuhengwang <yuhengw@andrew.cmu.edu>
2016-12-21 17:24:11 -0500

    Update Sum of Left Leaves.java

--- Easy/Sum of Left Leaves.java
+++ Easy/Sum of Left Leaves.java
@@ -7,6 +7,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+//recursive 
 public class Solution {
     public int sumOfLeftLeaves(TreeNode root) {
         
@@ -34,6 +35,43 @@ public class Solution {
          ans += sumOfLeftLeaves(root.right);
         
          return ans;
+    }
+    
+
+}
+
+// iterative
+public class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+       int ans = 0;
+       Stack <TreeNode> nodes = new Stack <TreeNode> ();
+       if(root == null) {
+           return 0;
+       }
+       
+       nodes.push(root);
+       
+       while(!nodes.isEmpty()) {
+           TreeNode node = nodes.pop();
+           if(node == null){
+               continue;
+           }
+         
+           if(node.left != null){
+               if(node.left.left == null && node.left.right == null) {
+                   
+                   ans += node.left.val;
+               }
+               else{
+               nodes.push(node.left);
+           }
+           }
+           
+           nodes.push(node.right);
+       }
+       
+       return ans;
+       
     }
     
 
