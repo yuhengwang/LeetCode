7f2cc57376161c337105bf0356c83adf28da8e06
yuhengwang <yuhengw@andrew.cmu.edu>
2017-01-14 20:14:58 -0500

    Update Contain Duplicate.java

--- Easy/Contain Duplicate.java
+++ Easy/Contain Duplicate.java
@@ -14,3 +14,20 @@ public class Solution {
         
     }
 }
+
+
+// time: O(N), space O(1)
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+       Arrays.sort(nums);
+       
+       for(int i=0; i< nums.length -1 ; i++) {
+           if(nums[i] == nums[i+1]) {
+               return true;
+           }
+       }
+     
+        return false;
+    }
+}
