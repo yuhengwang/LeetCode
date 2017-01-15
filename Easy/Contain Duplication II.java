9c7f615589be488e5da4f4ea13354136f016e55b
yuhengwang <yuhengw@andrew.cmu.edu>
2017-01-14 21:56:55 -0500

    Update Contain Duplication II.java

--- Easy/Contain Duplication II.java
+++ Easy/Contain Duplication II.java
@@ -15,3 +15,22 @@ public class Solution {
         
     }
 }
+
+// a pipe with length k
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        
+      HashSet <Integer> set = new HashSet <Integer> ();
+      
+      for(int i= 0; i< nums.length; i++) {
+          if(i > k) {
+              set.remove(nums[i-k-1]);
+          }
+          
+          if(!set.add(nums[i])) {
+              return true;
+          }
+      }
+      return false;  
+    }
+}
