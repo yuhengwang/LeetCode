40109fb48319fe42d9f7bc33968259dc7d644afb
yuhengwang <yuhengw@andrew.cmu.edu>
2017-01-16 16:08:34 +1030

    Update Pascal's Triangle.java

--- Easy/Pascal's Triangle.java
+++ Easy/Pascal's Triangle.java
@@ -41,3 +41,30 @@ public class Solution {
         
     }
 }
+
+
+//
+public class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        
+        List < Integer> row = new ArrayList <Integer> ();
+        List <List<Integer>> res= new ArrayList<List<Integer>> ();
+        
+        for(int i=0; i < numRows ; i++) {
+           
+            row.add(1);
+            
+            for(int j=i-1; j>0;j--){
+                row.set(j,row.get(j)+row.get(j-1));
+            }
+            
+            
+            res.add(new ArrayList <Integer>(row));
+            
+            
+        }
+        
+        return res;
+        
+    }
+}
