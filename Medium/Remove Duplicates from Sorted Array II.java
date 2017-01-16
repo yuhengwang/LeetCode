742a2f576f20476d57cd946dce4d63958f3c75f9
yuhengwang <yuhengw@andrew.cmu.edu>
2017-01-16 19:42:50 +1030

    Create Remove Duplicates from Sorted Array II.java

---
+++ Medium/Remove Duplicates from Sorted Array II.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        
+        if(nums.length < 2) {
+            return nums.length;
+        }
+        
+        int i = 1;
+        int j = 0;
+        int num = 1;
+        
+        while( i < nums.length) {
+            
+            if(nums[i] == nums[j]) {
+                
+                num++;
+                if(num < 3){
+                    j++;
+                    nums[j] = nums[i];
+                   
+                }
+                i++;
+                
+            }else{
+                num = 1;
+                j++;
+                nums[j] = nums[i];
+                i++;
+            }
+            
+        }
+        
+        return j+1;
+        
+        
+    }
+}
