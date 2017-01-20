aa51b3e4ccde42ab77ea024805b67503eedf646a
yuhengwang <yuhengw@andrew.cmu.edu>
2017-01-20 21:50:57 +1030

    Update Best Time to Buy and Sell Stock.java

--- Easy/Best Time to Buy and Sell Stock.java
+++ Easy/Best Time to Buy and Sell Stock.java
@@ -13,3 +13,16 @@ public class Solution {
         return profit;
     }
 }
+
+//Kadane's Algorithm
+public class Solution {
+    public int maxProfit(int[] prices) {
+      int maxCur = 0;
+      int maxSoFar = 0;
+      for(int i = 1; i < prices.length; i++) {
+           maxCur = Math.max(0, maxCur + prices[i] - prices[i - 1]);
+           maxSoFar = Math.max(maxSoFar, maxCur);
+      }
+      return maxSoFar;
+    }
+}
