f062dc0e1012f2a1598356e34e6d30265d0dfe72
yuhengwang <yuhengw@andrew.cmu.edu>
2017-01-22 16:38:34 +1030

    Update Binary Tree Postorder Traversal.java

--- Easy/Binary Tree Postorder Traversal.java
+++ Easy/Binary Tree Postorder Traversal.java
@@ -20,3 +20,34 @@ public class Solution {
         res.add(root.val);
     }
 }
+
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: Postorder in ArrayList which contains node values.
+     */
+     // divide and conquer
+    public ArrayList<Integer> postorderTraversal(TreeNode root) {
+        // write your code here
+        ArrayList<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+     
+        //return
+        
+        // divide
+        ArrayList<Integer> left = postorderTraversal(root.left);
+        ArrayList<Integer> right = postorderTraversal(root.right);
+        if (left != null) {
+            res.addAll(left);
+        }
+        if (right != null) {
+            res.addAll(right);
+        }
+        res.add(root.val);
+        return res;
+        
+    }
+   
+}
