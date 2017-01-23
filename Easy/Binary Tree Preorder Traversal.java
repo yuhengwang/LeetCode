2e07e6341a03d1af7f1f19ddbc106a0ee7057897
yuhengwang <yuhengw@andrew.cmu.edu>
2017-01-23 21:45:58 +1030

    Update Binary Tree Preorder Traversal.java

--- Easy/Binary Tree Preorder Traversal.java
+++ Easy/Binary Tree Preorder Traversal.java
@@ -25,3 +25,25 @@ public class Solution {
         return res;
     }
 }
+
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: Preorder in ArrayList which contains node values.
+     */
+    public ArrayList<Integer> preorderTraversal(TreeNode root) {
+        // divide and conquer
+        ArrayList<Integer> res = new ArrayList<>();
+        if(root == null) {
+            return res;
+        }
+        res.add(root.val);
+     
+        ArrayList<Integer>left = preorderTraversal(root.left);
+        ArrayList<Integer>right = preorderTraversal(root.right);
+        res.addAll(left);
+        res.addAll(right);
+        return res;
+      
+    }
+}
