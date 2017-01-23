2692644b2557d20d526620bf844d6044d32584dc
yuhengwang <yuhengw@andrew.cmu.edu>
2017-01-23 21:18:45 +1030

    Create Binary Tree Inorder Traversal.java

---
+++ Easy/Binary Tree Inorder Traversal.java
@@ -0,0 +1,25 @@
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: Inorder in ArrayList which contains node values.
+     */
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+        // write your code here
+        Stack<TreeNode> stack = new Stack<>();
+        ArrayList<Integer> res = new ArrayList<>();
+        TreeNode current = root;
+        if (root == null) {
+            return res;
+        }
+        while (!stack.empty() || current != null) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            res.add(current.val);
+            current = current.right;
+        }
+        return res;
+    }
+}
