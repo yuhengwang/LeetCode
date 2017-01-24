e7e190240b5c0d7bbf49ad61a7cd485a99cb15da
yuhengwang <yuhengw@andrew.cmu.edu>
2017-01-24 22:14:18 +1030

    Update Balanced Binary Tree.java

--- Easy/Balanced Binary Tree.java
+++ Easy/Balanced Binary Tree.java
@@ -44,3 +44,48 @@ public class Solution {
         return result;
     }
 }
+// without ResultType
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if this Binary tree is Balanced, or false.
+     */
+    boolean isbalance = true;
+    public boolean isBalanced(TreeNode root) {
+        // write your code here
+        if (root == null) {
+            return true;
+        }
+        helper(root);
+        return isbalance;
+    }
+    // return depth of subtree
+    public int helper(TreeNode root) {
+        if (root == null) {
+           return 0;
+        }
+        if (root.left == null && root.right == null) {
+           return 1;
+        }
+        int left = helper(root.left);
+        int right = helper(root.right);
+        if (Math.abs(left - right) > 1 || left == -1 || right == -1) {
+            isbalance = false;
+        }
+        return Math.max(left,right) + 1;
+        
+       
+    }
+
+}
