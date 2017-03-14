b667b671d877d28fbddbbb7caa4838b8f37aef62
yuhengwang <yuhengw@andrew.cmu.edu>
2017-03-14 15:38:48 +1030

    Update Copy List with random pointers.java

--- Medium/Copy List with random pointers.java
+++ Medium/Copy List with random pointers.java
@@ -56,3 +56,49 @@ public class Solution {
         return map.get(head);
     }
 }
+//another method make use of linkedlist
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class Solution {
+    /**
+     * @param head: The head of linked list with a random pointer.
+     * @return: A new head of a deep copy of the list.
+     */
+    public RandomListNode copyRandomList(RandomListNode head) {
+        // write your code here
+        if (head == null) {
+            return null;
+        }
+        HashMap<RandomListNode, RandomListNode> map = new HashMap<>();
+        RandomListNode dummy = new RandomListNode(0);
+        RandomListNode pre = dummy;
+        RandomListNode newNode = dummy;
+        while (head != null) {
+            if (!map.containsKey(head)) {
+                newNode =  new RandomListNode(head.label);
+                map.put(head, newNode);
+            } else {
+                newNode = map.get(head);
+            }
+            pre.next = newNode;
+            if (head.random != null) {
+                if (!map.containsKey(head.random)) {
+                    newNode.random = new RandomListNode(head.random.label);
+                    map.put(head.random, newNode.random);
+                } else {
+                    newNode.random = map.get(head.random);
+                }
+            }
+            
+            head = head.next;
+            pre = newNode;
+        }
+        return dummy.next;
+}
+}
