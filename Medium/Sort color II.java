08377e1c54ea9d5b19096a66e2ccdd5d92269434
yuhengwang <yuhengw@andrew.cmu.edu>
2017-03-15 19:03:59 +1030

    Update Sort color II.java

--- Medium/Sort color II.java
+++ Medium/Sort color II.java
@@ -46,3 +46,46 @@ class Solution {
         colors[j] = temp;
     }
 }
+//rainbow sort
+class Solution {
+    /**
+     * @param colors: A list of integer
+     * @param k: An integer
+     * @return: nothing
+     */
+    public void sortColors2(int[] colors, int k) {
+        // write your code here
+        if (colors == null || colors.length == 0 || k < 0) {
+            return;
+        }
+        rainbowSort(colors, 0, colors.length - 1, 1, k);
+    }
+    public void rainbowSort(int[] colors, int left, int right, int colorFrom, int colorTo) {
+        if (left >= right) {
+            return;
+        }
+        if (colorFrom >= colorTo) {
+            return;
+        }
+        int l = left;
+        int r = right;
+        int colorMid = (colorTo + colorFrom) / 2;
+        while (l < r) {
+            while (l < r && colors[l] <= colorMid) {
+                l++;
+            }
+            while (l < r && colors[r] > colorMid) {
+                r--;
+            }
+            if (l < r) {
+            int temp = colors[r];
+            colors[r] = colors[l];
+            colors[l] = temp;
+            l++;
+            r--;
+            }
+        }
+        rainbowSort(colors, left, r, colorFrom, colorMid);
+        rainbowSort(colors, l, right, colorMid + 1, colorTo);
+    }
+}
