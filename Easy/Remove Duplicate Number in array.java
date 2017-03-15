d91789a86ddcd590cb215206fc10c0f3ca900b04
yuhengwang <yuhengw@andrew.cmu.edu>
2017-03-15 12:43:21 +1030

    Update Remove Duplicate Number in array.java

--- Easy/Remove Duplicate Number in array.java
+++ Easy/Remove Duplicate Number in array.java
@@ -4,7 +4,7 @@ public class Solution {
      * @return the number of unique integers
      */
     public int deduplication(int[] nums) {
-        // Write your code here
+        // Write your code here O(nlogn)
         if (nums == null || nums.length == 0) {
             return 0;
         }
@@ -19,6 +19,27 @@ public class Solution {
             
         }
         return len + 1;
+        
+        
+    }
+}
+                                 //O(n) time & space
+ public class Solution {
+    /**
+     * @param nums an array of integers
+     * @return the number of unique integers
+     */
+    public int deduplication(int[] nums) {
+        // Write your code here
+       HashMap<Integer, Boolean> maps = new HashMap<>();
+       for (int i = 0; i < nums.length; i++) {
+           maps.put(nums[i], true);
+       }
+       int result = 0;
+       for (Map.Entry<Integer, Boolean> entries: maps.entrySet()) {
+           nums[result++] = entries.getKey();
+       }
+       return result;
         
         
     }
