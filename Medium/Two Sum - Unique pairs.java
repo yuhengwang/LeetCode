00ea8b22b0ddf56ac659c05ae65863043071d438
yuhengwang <yuhengw@andrew.cmu.edu>
2017-03-15 17:04:11 +1030

    Update Two Sum - Unique pairs.java

--- Medium/Two Sum - Unique pairs.java
+++ Medium/Two Sum - Unique pairs.java
@@ -27,3 +27,41 @@ public class Solution {
         return num;
     }
 }
+// time complexity O(nlogn), space complexity o(n)
+public class Solution {
+    /**
+     * @param nums an array of integer
+     * @param target an integer
+     * @return an integer
+     */
+    public int twoSum6(int[] nums, int target) {
+        // Write your code here
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length - 1;
+        int num = 0;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == target) {
+                num++;
+                left++;
+                right--;
+                while (left < right && nums[right] == nums[right + 1]) {
+                    right--;
+                }
+                while (left < right && nums[left] == nums[left - 1]) {
+                    left++;
+                }
+            } else if (sum > target) {
+                right--;
+            } else {
+                left++;
+            }
+            
+        }
+        return num;
+    }
+}
