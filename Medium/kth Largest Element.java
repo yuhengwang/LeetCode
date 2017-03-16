235fc270ca4efb222617c94daf6c8f780c26c623
yuhengwang <yuhengw@andrew.cmu.edu>
2017-03-16 14:08:10 +1030

    Create kth Largest Element.java

---
+++ Medium/kth Largest Element.java
@@ -0,0 +1,45 @@
+class Solution {
+    /*
+     * @param k : description of k
+     * @param nums : array of nums
+     * @return: description of return
+     */
+    public int kthLargestElement(int k, int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        return quickSelect(nums, 0, nums.length - 1, k);
+    }
+    private int quickSelect(int[] nums, int start, int end, int k) {
+        if (start == end) {
+            return nums[start];
+        }
+        int i = start;
+        int j = end;
+        int pivot = nums[(i + j) / 2];
+        while (i <= j) {
+            while (i <= j && nums[i] > pivot) {
+                i++;
+            }
+            while (i <= j && nums[j] < pivot) {
+                j--;
+            }
+            if (i <= j) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        //
+        if (start + k - 1 >= i) {
+            return quickSelect(nums, i, end, k - (i - start));
+        }
+        if (start + k - 1 <= j) {
+            return quickSelect(nums, start, j, k);
+        }
+        return nums[i - 1];
+    }
+};
