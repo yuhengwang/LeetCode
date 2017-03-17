fd0d030ecd2280ce506e3066e37ba877d9cb1819
yuhengwang <yuhengw@andrew.cmu.edu>
2017-03-18 00:46:56 +1030

    Update Merge k Sorted Lists.java

--- Medium/Merge k Sorted Lists.java
+++ Medium/Merge k Sorted Lists.java
@@ -9,6 +9,7 @@
  *     }
  * }
  */ 
+// divide and conquer
 public class Solution {
     /**
      * @param lists: a list of ListNode
@@ -49,6 +50,45 @@ public class Solution {
         }
         if (right != null) {
             tail.next = right;
+        }
+        return dummy.next;
+    }
+}
+//heap
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param lists: a list of ListNode
+     * @return: The head of one sorted list.
+     */
+    public ListNode mergeKLists(List<ListNode> lists) {  
+        // write your code here
+        if (lists == null || lists.size() == 0) {
+            return null;
+        }
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int i = 0; i < lists.size(); i++) {
+            ListNode temp = lists.get(i);
+            while (temp != null) {
+                queue.offer(temp.val);
+                temp = temp.next;
+            }
+        }
+        ListNode result = new ListNode(0);
+        ListNode dummy = result;
+        while (!queue.isEmpty()) {
+            result.next = new ListNode(queue.poll());
+            result = result.next;
         }
         return dummy.next;
     }
