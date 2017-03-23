f76b863a866b3173e9d4e5829fbaa92e91ac9593
yuhengwang <yuhengw@andrew.cmu.edu>
2017-03-23 17:13:06 +1030

    Create Partition List.java

---
+++ Easy/Partition List.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @param x: an integer
+     * @return: a ListNode 
+     */
+    public ListNode partition(ListNode head, int x) {
+        // write your code here
+        ListNode leftDummy = new ListNode(0);
+        ListNode rightDummy = new ListNode(0);
+        ListNode left = leftDummy;
+        ListNode right = rightDummy;
+        while (head != null) {
+            if (head.val < x) {
+                left.next = head;
+                left = head;
+            } else {
+                right.next = head;
+                right = head;
+            }
+            head = head.next;
+            
+        }
+        right.next = null;
+        left.next = rightDummy.next;
+        return leftDummy.next;
+    }
+}
