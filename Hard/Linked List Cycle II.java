77ea349d0682f449ddbea75619765c164342c7a4
yuhengwang <yuhengw@andrew.cmu.edu>
2017-03-23 17:44:18 +1030

    Create Linked List Cycle II.java

---
+++ Hard/Linked List Cycle II.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @return: The node where the cycle begins. 
+     *           if there is no cycle, return null
+     */
+    public ListNode detectCycle(ListNode head) {  
+        if (head == null) {
+            return null;
+        }
+        // write your code here
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != slow) {
+            if (fast == null || fast.next == null) {
+                return null;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        while (head != slow.next) {
+            head = head.next;
+            slow = slow.next;
+        }
+        return head;
+    }
+}
