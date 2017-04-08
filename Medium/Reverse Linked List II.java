22c559c8c52b65881c612ef8810fca7dc25211fb
yuhengwang <yuhengw@andrew.cmu.edu>
2017-04-08 17:43:39 +0930

    Update Reverse Linked List II.java

--- Medium/Reverse Linked List II.java
+++ Medium/Reverse Linked List II.java
@@ -55,3 +55,50 @@ public class Solution {
         
     }
 }
+
+// more brief way
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param ListNode head is the head of the linked list 
+     * @oaram m and n
+     * @return: The head of the reversed ListNode
+     */
+    public ListNode reverseBetween(ListNode head, int m , int n) {
+        // write your code
+        // first find m node
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        head = dummy;
+        for (int i = 1; i < m; i++) {
+            if (head == null) {
+                return null;
+            }
+            head = head.next;
+        }
+        ListNode preM = head;
+        ListNode mNode = head.next; 
+        ListNode postnNode = mNode.next;  //cur
+        ListNode nNode = mNode; //prev
+        for (int i = m; i < n; i++) {
+            ListNode temp = postnNode.next;
+            postnNode.next = nNode;
+            nNode = postnNode;
+            postnNode = temp;
+        }
+        // connect
+        mNode.next = postnNode;
+        preM.next = nNode;
+        return dummy.next;
+    }
+}
