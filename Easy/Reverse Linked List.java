9ea7203cd1fe273b606dae48904c66d229c40c93
yuhengwang <yuhengw@andrew.cmu.edu>
2017-04-08 15:40:00 +0930

    Create Reverse Linked List.java

---
+++ Easy/Reverse Linked List.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The head of linked list.
+     * @return: The new head of reversed linked list.
+     */
+    public ListNode reverse(ListNode head) {
+        // write your code here
+        
+        ListNode prev = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode temp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = temp;
+        }
+        return prev;
+    }
+}
