e0423e4a48432e85c78d506d962348898aea7f94
yuhengwang <yuhengw@andrew.cmu.edu>
2017-04-24 23:46:21 +0930

    Create Unique Paths.java

---
+++ Algorithm/Dynamic Programming/Unique Paths.java
@@ -0,0 +1,25 @@
+public class Solution {
+    /**
+     * @param n, m: positive integer (1 <= n ,m <= 100)
+     * @return an integer
+     */
+    public int uniquePaths(int m, int n) {
+        // write your code here 
+        //state: how many paths to start from top left to i,j
+        int[][] f = new int[m][n];
+        //initialize
+        for (int i = 0; i < m; i++) {
+            f[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            f[0][j] = 1;
+        }
+        //loop
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                f[i][j] = f[i - 1][j] + f[i][j - 1];
+            }
+        }
+        return f[m - 1][n - 1];
+    }
+}
