9f41ed27fe01a89db6b565cef6c099a1a103b2d5
yuhengwang <yuhengw@andrew.cmu.edu>
2017-07-24 14:10:38 +0800

    Update Count Numbers with Unique Digits.java

--- Algorithm/Backtracking/Count Numbers with Unique Digits.java
+++ Algorithm/Backtracking/Count Numbers with Unique Digits.java
@@ -1,4 +1,4 @@
-//n 相当于数字的位数，但是 
+//n 相当于数字的位数，但是当n为0时，要特殊考虑，用一个boolean array来过滤出现重复数字的数，用digit 的长度大于当前位数来判断是否进入下一步循环
 public class Solution {
     int count = 0;
     public int countNumbersWithUniqueDigits(int n) {
