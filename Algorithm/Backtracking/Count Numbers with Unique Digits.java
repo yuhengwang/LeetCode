5d0588ef34a70d81134a6450baa482a6cc16e723
yuhengwang <yuhengw@andrew.cmu.edu>
2017-07-24 14:08:04 +0800

    Create Count Numbers with Unique Digits.java

---
+++ Algorithm/Backtracking/Count Numbers with Unique Digits.java
@@ -0,0 +1,34 @@
+public class Solution {
+    int count = 0;
+    public int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        boolean[] used = new boolean[10]; //0 - 9
+        for (int i = 1; i <= n; i++) {
+            dfs(i, used, "");
+        }
+        return count;
+    }
+    public void dfs(int place, boolean[] used, String digit) {
+        if (digit.length() > place) {
+            return;
+        }
+        if (digit.length() == place) {
+            count++;
+            return;
+        }
+        for (int i = 0; i <= 9; i++) {
+            if (digit.length() == 0 && i == 0 && place > 1) {
+                continue;
+            }
+            if (used[i]) {
+                continue;
+            }
+            used[i] = true;
+            dfs(place, used, digit + i);
+            digit = digit.substring(0, digit.length());
+            used[i] = false;
+        }
+    }
+}
