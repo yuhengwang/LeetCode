1bb936a4295ce0feb51cb1a5ecf6d6e80b64e222
yuhengwang <yuhengw@andrew.cmu.edu>
2017-08-03 15:22:47 +0800

    Update burst balloon.java

--- Algorithm/Dynamic Programming/burst balloon.java
+++ Algorithm/Dynamic Programming/burst balloon.java
@@ -1,3 +1,44 @@
+//int[][]dp : the maximum value get from ith to jth balloon, but ith and jth are also considered as potential burst balloon
+public class Solution {
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][n];
+        boolean[][] visited = new boolean[n][n];
+        return burst(dp, nums, visited, 0, n - 1);
+    }
+    public int burst(int[][] dp, int[] nums, boolean[][] visited, int i, int j) {
+        if (j < i) {
+            return 0;
+        }
+        if (visited[i][j]) {
+            return dp[i][j];
+        }
+        visited[i][j] = true;
+        if (i == j) {
+            int value = getValue(i - 1, nums) * getValue(i, nums) * getValue(i + 1, nums);
+            dp[i][j] = value;
+            return dp[i][j];
+        }
+        int max = Integer.MIN_VALUE;
+        for (int k = i; k <= j; k++) {
+            //the left boundary should not be k - 1 or k + 1but i - 1 and j + 1
+            int value = getValue(i - 1, nums) * getValue(j + 1, nums) * getValue(k, nums);   
+            max = Math.max(max, value + burst(dp, nums, visited, i, k - 1) + burst(dp, nums, visited, k + 1, j));
+        }
+        dp[i][j] = max;
+        return dp[i][j];
+    }
+    public int getValue(int i, int[] nums) {
+        if (i < 0 || i >= nums.length) {
+            return 1;
+        } else {
+            return nums[i];
+        }
+    }
+    
+}
+
+
 // int[][] dp the maximum value we can get if burst balloon from ith balloon to jth balloon, ith and jth are the left and
 right boundary, they are not considered as potential bursted balloon
 public class Solution {
