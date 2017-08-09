352004dfef01f1299285ed5c2d6151c44e12d012
yuhengwang <yuhengw@andrew.cmu.edu>
2017-08-09 10:31:52 +0800

    Create Linked List/Merge Two Sorted List.java

---
+++ Algorithm/Linked List/Merge Two Sorted List.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                dummy.next = new ListNode(l1.val);
+                l1 = l1.next;
+            } else {
+                dummy.next = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            dummy = dummy.next;
+        }
+        while (l1 != null) {
+            dummy.next = new ListNode(l1.val);
+            l1 = l1.next;
+            dummy = dummy.next;
+        }
+        while (l2 != null) {
+            dummy.next = new ListNode(l2.val);
+            l2 = l2.next;
+            dummy = dummy.next;
+        }
+        return head.next;
+    }
+}
