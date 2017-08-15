e72c5464aff519a556d5075ddc9ba25e2304ebb1
yuhengwang <yuhengw@andrew.cmu.edu>
2017-08-15 22:35:22 +0800

    Create Read N Characters Given Read4.java

---
+++ Algorithm/String/Read N Characters Given Read4.java
@@ -0,0 +1,25 @@
+/* The read4 API is defined in the parent class Reader4.
+      int read4(char[] buf); */
+
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    public int read(char[] buf, int n) {
+        boolean eof = false;
+        int total = 0;
+        char[] temp = new char[4];
+        while (!eof && total < n) {
+            int count = read4(temp);
+            eof = count < 4;
+            count = Math.min(count, n - total);
+            for (int i = 0; i < count; i++) {
+                buf[total++] = temp[i];
+            }
+        }
+        
+        return total; 
+    }
+}
