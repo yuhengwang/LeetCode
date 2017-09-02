db5617168b8d788c388d4f2b9340a8a97a6db053
yuhengwang <yuhengw@andrew.cmu.edu>
2017-09-01 23:20:34 -0400

    Create Design Tic-Tac-Toe.java

---
+++ Algorithm/Data structure Design/Design Tic-Tac-Toe.java
@@ -0,0 +1,43 @@
+class TicTacToe {
+    private int[] rows;
+    private int[] cols;
+    private int diag;
+    private int antiDiag;
+    
+    /** Initialize your data structure here. */
+    public TicTacToe(int n) {
+        rows = new int[n];
+        cols = new int[n];
+    }
+    
+    /** Player {player} makes a move at ({row}, {col}).
+        @param row The row of the board.
+        @param col The column of the board.
+        @param player The player, can be either 1 or 2.
+        @return The current winning condition, can be either:
+                0: No one wins.
+                1: Player 1 wins.
+                2: Player 2 wins. */
+    public int move(int row, int col, int player) {
+       int toAdd = player == 1 ? 1 : -1;
+       if (row == col) {
+           diag += toAdd;
+       }
+       if (col == cols.length - row - 1) {
+           antiDiag += toAdd;
+       }
+       rows[row] += toAdd;
+       cols[col] += toAdd;
+       if (Math.abs(rows[row]) == rows.length || Math.abs(cols[col]) == rows.length ||
+           Math.abs(diag) == rows.length || Math.abs(antiDiag) == rows.length) {
+           return player;
+       }
+       return 0;
+    }
+}
+
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * TicTacToe obj = new TicTacToe(n);
+ * int param_1 = obj.move(row,col,player);
+ */
