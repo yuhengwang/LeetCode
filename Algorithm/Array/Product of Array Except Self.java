2def826a761d953498fbe2abe1d50ae091f14b67
yuhengwang <yuhengw@andrew.cmu.edu>
2017-09-03 16:23:05 -0400

    Create Product of Array Except Self.java

---
+++ Algorithm/Array/Product of Array Except Self.java
@@ -0,0 +1,16 @@
+从前往后遍历前缀积，从后往前遍历同理。最后ith 的乘积是除了自己之外的的数
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        res[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
+    }
+}
