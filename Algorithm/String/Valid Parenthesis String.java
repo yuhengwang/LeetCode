233b78ff29003ac2f50f0a0f66f81a8d84b5a298
yuhengwang <yuhengw@andrew.cmu.edu>
2017-09-17 23:41:23 -0400

    Create Valid Parenthesis String.java

---
+++ Algorithm/String/Valid Parenthesis String.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int low = 0;
+        int high = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                low++;
+                high++;
+            } else if (s.charAt(i) == ')') {
+                if (low > 0)
+                    low--;
+                high--;
+            } else {
+                if (low > 0) {
+                    low--;
+                }
+                high++;
+            }
+            if (high < 0)   return false; //high is used to handle low < 0 problem
+        }
+        return low == 0; //low > 0 means "(" is more than ")"
+    }
+}
