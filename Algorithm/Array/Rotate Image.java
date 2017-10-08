8e211bb88dfd03bd478fad9663a65e5544890a67
yuhengwang <yuhengw@andrew.cmu.edu>
2017-10-08 17:04:57 -0400

    Create  Rotate Image.java

---
+++ Algorithm/Array/Rotate Image.java
@@ -0,0 +1,25 @@
+transpose and do symmetric mirror on each row!
+class Solution {
+    public void rotate(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) {
+            return;
+        }
+        //transpose
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        //symmetric
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix[0].length - 1 - j];
+                matrix[i][matrix[0].length - 1 - j] = temp;
+            }
+        }
+    
+    }
+}
