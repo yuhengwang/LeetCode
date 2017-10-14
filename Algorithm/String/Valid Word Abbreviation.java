f8a5ae30af1dbb2d49a512d1dcda69f907794432
yuhengwang <yuhengw@andrew.cmu.edu>
2017-10-14 12:39:00 -0400

    Create Valid Word Abbreviation.java

---
+++ Algorithm/String/Valid Word Abbreviation.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean validWordAbbreviation(String word, String abbr) {
+        int i = 0, j = 0;
+        while (i < word.length() && j < abbr.length()) {
+            if (word.charAt(i) == abbr.charAt(j)) {
+                i++;
+                j++;
+                continue;
+            }
+            //<=0 0 cannot be the first integer
+            if (abbr.charAt(j) <= '0' || abbr.charAt(j) > '9') {
+                return false;
+            }
+            int start = j;
+            while (j < abbr.length() && abbr.charAt(j) >= '0' && abbr.charAt(j) <= '9') {
+                j++;
+            }
+            int skip = Integer.parseInt(abbr.substring(start, j));
+            i += skip;
+        }
+        return i == word.length() && j == abbr.length();
+    }
+}
