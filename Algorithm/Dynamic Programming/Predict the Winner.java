0517cb184576526d8b06b2d78a63842dcaf43fd8
yuhengwang <yuhengw@andrew.cmu.edu>
2017-10-15 19:21:39 -0400

    Update Predict the Winner.java

--- Algorithm/Dynamic Programming/Predict the Winner.java
+++ Algorithm/Dynamic Programming/Predict the Winner.java
@@ -1,3 +1,20 @@
+The dp[i][j] saves how much more scores that the first-in-action player will get from i to j than the second player. 
+ First-in-action means whomever moves first. You can still make the code even shorter but I think it looks clean in this way.
+public boolean PredictTheWinner(int[] nums) {
+    int n = nums.length;
+    int[][] dp = new int[n][n];
+    for (int i = 0; i < n; i++) { dp[i][i] = nums[i]; }
+    for (int len = 1; len < n; len++) {
+        for (int i = 0; i < n - len; i++) {
+            int j = i + len;
+            dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+        }
+    }
+    return dp[0][n - 1] >= 0;
+}
+
+
+
 区间型dp。
 最后Return的东西是为了要player 2也选择最大值。
 time complexity o(2^n), space complexity o(1)
