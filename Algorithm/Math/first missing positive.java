4fe128d1472b1e037ee46780c062f47995243c86
yuhengwang <yuhengw@andrew.cmu.edu>
2017-12-01 19:02:50 -0500

    Create first missing positive.java

---
+++ Algorithm/Math/first missing positive.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int firstMissingPositive(int[] A) {
+        int i = 0;
+        while(i < A.length){
+            if(A[i] == i+1 || A[i] <= 0 || A[i] > A.length) i++;
+            //put the value of A[i] to its specific position!
+            else if(A[A[i]-1] != A[i]) swap(A, i, A[i]-1);
+            else i++;
+        }
+        i = 0;
+        while(i < A.length && A[i] == i+1) i++;
+        //A[i+1] may be negative or larger than A.length
+        return i+1;
+    }
+    
+    private void swap(int[] A, int i, int j){
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+}
